01687a23f45f0b8d7f46507059f4547dc1ed061b
Ngoc Dung Huynh <63529910+parkerhuynh@users.noreply.github.com>
2022-05-12 14:30:05 +1000

    Update metrics.py

--- training/metrics.py
+++ training/metrics.py
@@ -1,29 +1,21 @@
-from sklearn.metrics import accuracy_score, roc_auc_score
-from torch import sigmoid
-from numpy import round
+from tensorflow import keras
 
-def acc_probcovid(preds, labels):
-    preds = sigmoid(preds[:, 0]).numpy()
-    labels = labels[:, 0].numpy()
-    return accuracy_score(labels, round(preds))
-
-def acc_probseverecovid(preds, labels):
-    mask = labels[:, 0].numpy()
-    preds = sigmoid(preds[:, 1]).numpy()
-    preds = preds[mask > 0.5]
-    labels = labels[:, 1].numpy()
-    labels = labels[mask > 0.5]
-    return accuracy_score(labels, round(preds))
-
-def roc_probcovid(preds, labels):
-    preds = preds[:, 0].numpy()
-    labels = labels[:, 0].numpy()
-    return roc_auc_score(labels, preds)
-
-def roc_probseverecovid(preds, labels):
-    mask = labels[:, 0].numpy()
-    preds = preds[:, 1].numpy()
-    preds = preds[mask > 0.5]
-    labels = labels[:, 1].numpy()
-    labels = labels[mask > 0.5]
-    return roc_auc_score(labels, preds)
+class Metric(keras.callbacks.Callback):
+    def __init__(self, dataset, model_name, target, artifact_dir):
+        super().__init__()
+        self.dataset = dataset
+        self.model_name = model_name
+        self.target = target
+        self.artifact_dir = artifact_dir
+        self.max_auc = 0
+        self.output = {"val_loss": [], "val_auc": [], "val_acc" : []}
+        
+    def on_epoch_end(self, epoch: int, logs=None):
+        result = self.model.evaluate(self.dataset)
+        val_loss, val_auc, val_acc = result
+        self.output["val_loss"].append(val_loss)
+        self.output["val_auc"].append(val_auc)
+        self.output["val_acc"].append(val_acc)
+        if val_auc >= self.max_auc:
+            self.max_auc = val_auc
+            self.model.save(f"{self.artifact_dir}/{self.model_name}_{self.target }.h5")
