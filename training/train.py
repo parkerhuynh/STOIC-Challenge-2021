4a7c313bd18c5c7b54d275af2e54a3dcea2c3fda
Ngoc Dung Huynh <63529910+parkerhuynh@users.noreply.github.com>
2022-05-12 14:31:08 +1000

    Update train.py

--- training/train.py
+++ training/train.py
@@ -10,6 +10,7 @@ import efficientnet_3D.tfkeras as efn
 from classification_models_3D.tfkeras import Classifiers  
 from tensorflow import keras
 import tensorflow as tf
+from metrics import Metric
 
 
 CONFIGFILE = "/opt/train/config/baseline.json"
@@ -49,25 +50,6 @@ def Network(model_name, width=240, height= 240, depth=240):
                         outputs = outputs)
     return model
 
-class Metric(keras.callbacks.Callback):
-    def __init__(self, dataset, model_name, target, artifact_dir):
-        super().__init__()
-        self.dataset = dataset
-        self.model_name = model_name
-        self.target = target
-        self.artifact_dir = artifact_dir
-        self.max_auc = 0
-        self.output = {"val_loss": [], "val_auc": [], "val_acc" : []}
-        
-    def on_epoch_end(self, epoch: int, logs=None):
-        result = self.model.evaluate(self.dataset)
-        val_loss, val_auc, val_acc = result
-        self.output["val_loss"].append(val_loss)
-        self.output["val_auc"].append(val_auc)
-        self.output["val_acc"].append(val_acc)
-        if val_auc >= self.max_auc:
-            self.max_auc = val_auc
-            self.model.save(f"{self.artifact_dir}/{self.model_name}_{self.target }.h5")
         
 def train(config, train_ds, valid_ds, valid_ds_severe, target, artifact_dir):
     gpus = tf.config.list_logical_devices('GPU')
